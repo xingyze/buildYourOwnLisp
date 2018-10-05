fc9d2321ac756a2a08165308f87d3c10e225ba13
xingyze <xyz1069084855@gmail.com>
2018-10-05 23:39:40 +0800

    supported complie in windows

--- repl.c
+++ repl.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* If we are compilling on windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2038];
+
+/* Fake readline function */
+char* readline(char* prompt)
+{
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer) + 1);// +1 for '\0'
+	strcpy(cpy, buffer);
+	spy[strlen(cpy)-1] = '\0';
+	
+	return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unusedd){}
+
+/* Otherwise include the editline headers */
+
+#else
 #include <editline/history.h>
 #include <editline/readline.h>
-
+#endif
 int main(int argc, char** argv)
 {
 	/* Print Version and Exit Information. */
