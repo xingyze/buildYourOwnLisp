ab37f1dcf7e5356c303ab3f4925b4174e7d5e9a9
xingyze <xyz1069084855@gmail.com>
2018-10-05 23:20:11 +0800

    fit linux command line, make our lispy support arrow (up down lef right)

--- repl.c
+++ repl.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+#include <editline/history.h>
+#include <editline/readline.h>
 
 int main(int argc, char** argv)
 {
 	/* Print Version and Exit Information. */
-	puts("Lispy version 0.0.0.0.1");
+	puts("Lispy version 0.0.0.0.2");
 	puts("Press Ctrl+c to Exit\n");
 
 	/* In a never ending loop */
 	while (1)
 	{
 		/* Output our prompt */
-		fputs("lispy> ", stdout);
+		char* input = readline("lisp> ");
 		
-		/* Read a line of user input of maximum size 2048 */
-		fgets(input, 2048, stdin);
+		/* Add input to history */
+		add_history(input);
 
 		/* Echo input back to user */
-		printf("No you're a %s", input);
+		printf("No you're a %s\n", input);
+
+		/* Free retrieved input */
+		free(input);
 	}
 
 	return 0;
